c590408f0c9fe2c54738c26c9973b432fcd60c01
Alexander Perov <www.shurik_47@mail.ru>
2021-11-03 15:32:29 +0300

    'social network/lesson 13'

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://vectorlogoseek.com/wp-content/uploads/2018/12/premier-league-vector-logo.png'/>
+        </header>
+    )
+}
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <img src="https://tipik.ru/wp-content/uploads/2018/11/1316796242_67_www.nevseoboi.com_.ua_.jpg"
+                 alt="img"/>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    )
+}
